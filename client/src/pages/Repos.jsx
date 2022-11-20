48d3ac867d3074c05799cc56ac0cac0da425b3f6
David Amebley <davameb@gmail.com>
2022-11-20 03:01:23 +0200

    Insert Pagination Component

--- client/src/pages/Repos.jsx
+++ client/src/pages/Repos.jsx
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-// import { Pagination } from '@mui/material';
-// import Pagination from '@mui/material';
-// import Pagination from '@mui/material/Pagination';
 import Pagination from '@mui/material/Pagination';
-// import { Pagination } from '@mui/material';
 import {repos} from '../data';
 
 const Repos = () => {
@@ -12,20 +8,27 @@ const Repos = () => {
   const [page, setPage] = useState(1);
 
   const handlePageChange = (e, p) => {
-
+    setPage(p);
   }
+  const pageLimit = 3;
+  const dataLength = repos.length;
+  let pageOffset = ((pageLimit * page) - pageLimit + 1);
+  const paginationCount = Math.ceil(dataLength / pageLimit)
   useEffect(() => {
     
   }, []);
   return (
-    <div>
+    <div className='repos'>
       <ul className="content-list">
         { repos.map(repo => (
             // <Card key={repo.id} repo = {repo} />
             <li key={repo.id} className="link content-list-item" onClick={()=>navigate(`${repo.id}`)}>{repo.name}</li>
         )) }
       </ul>
-      <Pagination count={10} color='primary' onChange={handlePageChange}> </Pagination>
+      <div>
+        <h4>Current page: {page} Data Length: {dataLength} PageOffSet: {pageOffset} </h4>
+        <Pagination count={paginationCount} color='primary' onChange={handlePageChange}> </Pagination>
+      </div>
     </div>
   )
 }
