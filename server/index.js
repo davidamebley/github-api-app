54e931c3e21f094333de50e4f7715224e30dc3c7
David Amebley <davameb@gmail.com>
2022-11-21 00:06:14 +0200

    Set up GitHub strategy with Passport

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+
+const app = express()
+
+app.listen("5000", () => {
+    console.log("Server running on port 5000");
+})
