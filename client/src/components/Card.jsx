e49511ad5a7ecd99fcf79d17dcd078bfb1c4bf76
David Amebley <davameb@gmail.com>
2022-11-22 00:02:09 +0200

    Install Material UI Icons

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
 
 const Card = ({ user }) => {
   return (
@@ -7,7 +8,10 @@ const Card = ({ user }) => {
         <ul className="content-list">
             <li className="content-list-item">
                 <img src={user.photos[0].value} alt="user avatar" className="content-avatar" />
-                <p><b> {user.displayName} </b></p>
+                <div>
+                  <p><b> {user.displayName} </b></p> 
+                  <p><b><LocationOnIcon/> {user._json.location}</b></p>
+                </div>
             </li>
             <li className="content-list-item"><b>Email: </b>&nbsp; {user._json.email ? user._json.email : <b><i>Your email has not been set</i></b>}</li>
             <li className="content-list-item"><b> Total Public Repos: {user._json.public_repos}</b></li>
