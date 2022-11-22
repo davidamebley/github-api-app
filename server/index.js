46ba804121f3db8b4702c5d2e16fff86ca3cbf28
David Amebley <davameb@gmail.com>
2022-11-22 23:14:10 +0200

    Update styles

--- server/index.js
+++ server/index.js
@@ -38,6 +38,6 @@ app.use('/auth', authRoute);
 
 
 
-app.listen("5000", () => {
-    console.log("Server running on port 5000");
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 })
